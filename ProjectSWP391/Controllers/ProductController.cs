ac30573b0b6beaf1d424ca3a24fab77981945663
locpxhe140601 <locpxhe140601@fpt.edu.vn>
2023-06-04 23:40:45 +0700

    Commit Product
    List, Create Product

---
+++ ProjectSWP391/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using DEMOSWP391.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEMOSWP391.Controllers
+{
+    public class ProductController : Controller
+    {
+        swp391Context context = new swp391Context();
+        public IActionResult Index()
+        {
+            var lsProduct = context.Products.ToList(); 
+            return View(lsProduct);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var p = context.Products.Where(n => n.ProductId == id).FirstOrDefault();    
+            return View(p);
+        }
+
+        [HttpGet]
+        public ActionResult CreateProduct(Product product)
+        {
+
+            return View();
+        }
+    }
+}
