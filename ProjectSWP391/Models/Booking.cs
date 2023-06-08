40d77340260b16eb7b44c3ac334513b187e99abf
Longcodebad <Longcodebad@gmail.com>
2023-06-08 11:06:56 +0700

    Scaffording new DB

--- ProjectSWP391/Models/Booking.cs
+++ ProjectSWP391/Models/Booking.cs
@@ -6,15 +6,15 @@ namespace ProjectSWP391.Models
     public partial class Booking
     {
         public int BookingId { get; set; }
-        public int EmployeeId { get; set; }
+        public int? EmployeeId { get; set; }
         public int? CustomerId { get; set; }
-        public int? ShiftId { get; set; }
         public int? ServiceId { get; set; }
         public string Content { get; set; }
+        public DateTime? BookingDate { get; set; }
+        public int? Shift { get; set; }
 
-        public virtual Account? Customer { get; set; }
-        public virtual Account? Employee { get; set; }
-        public virtual Service? Service { get; set; }
-        public virtual Shift? Shift { get; set; }
+        public virtual Account Customer { get; set; }
+        public virtual Account Employee { get; set; }
+        public virtual Service Service { get; set; }
     }
 }
---
+++ ProjectSWP391/Models/IsWorking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectSWP391.Models
+{
+    public partial class IsWorking
+    {
+        public int WorkingId { get; set; }
+        public int AccountId { get; set; }
+        public int Status { get; set; }
+        public DateTime? WorkingDay { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
