089f46416c146ce3c7965b722f2dab05b78bb16a
locpx5120 <locpx5120@gmail.com>
2023-06-18 21:36:26 +0700

    CRUD Account, search

---
+++ ProjectSWP391/Controllers/CustomerAccountController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectSWP391.Models;
+
+namespace ProjectSWP391.Controllers
+{
+    public class CustomerAccountController : Controller
+    {
+        SWP391Context context = new SWP391Context();
+        //action show list account of customer
+        public IActionResult Index()
+        {
+            int role = 2;
+            //show all account of Customer have role = 2;
+            var lstCustomer = context.Accounts.Where(a => a.Role == role).ToList();
+            return View(lstCustomer);
+        }
+
+        //action show form create account
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //action logic processing create new account 
+        [HttpPost]
+        public ActionResult Create(Account account)
+        {
+            //check email exist
+            var existEmail = context.Accounts.FirstOrDefault(a => a.Email == account.Email);
+            if (existEmail != null)
+            {
+                ModelState.AddModelError("Email", "Email exist, please enter new email");
+                return View(account);
+            }
+            if (ModelState.IsValid)
+            {
+                account.Role = 2;
+                account.IsActive = true;
+                context.Accounts.Add(account);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+
+        //xem detail cua nhan vien
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var s = context.Accounts.Where(n => n.AccountId == id).FirstOrDefault();
+            return View(s);
+        }
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            var sID = context.Accounts.Find(id);
+            return View(sID);
+        }
+        [HttpGet]
+        public ActionResult Update(Account account)
+        {
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var s = context.Accounts.Where(n => n.AccountId == id).FirstOrDefault();
+            return View(s);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(Account account)
+        {
+            context.Accounts.Remove(account);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ProjectSWP391/Controllers/ProductManagementController.cs
+++ ProjectSWP391/Controllers/ProductManagementController.cs
@@ -8,21 +8,56 @@ namespace DEMOSWP391.Controllers
         SWP391Context context = new SWP391Context();
         public IActionResult Index()
         {
-            var lsProduct = context.Products.ToList(); 
+            var lsProduct = context.Products.Include(p => p.Pcategory).ToList();
             return View(lsProduct);
         }
 
         public ActionResult Details(int id)
         {
-            var p = context.Products.Where(n => n.ProductId == id).FirstOrDefault();    
+            var p = context.Products.Where(n => n.ProductId == id).FirstOrDefault();
             return View(p);
         }
-
-        [HttpGet]
-        public ActionResult CreateProduct(Product product)
+        //LOAD ALL CATEGORY
+        public List<ProductCategory> GetProductCategories()
         {
-
+            var list = new List<ProductCategory>();
+            try
+            {
+                list = context.ProductCategories.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return list;
+        }
+        public ActionResult Create()
+        {
+            var categories = GetProductCategories();
+            ViewBag.ListPCategory = new SelectList(categories, "PcategoryId", "PcategoryName");
             return View();
+        }
+
+        //create a product
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            //check product name exist
+            //var existProductName = context.Products.FirstOrDefault(a => a.ProductName == p.ProductName);
+            //if (existProductName != null)
+            //{
+
+            //    return RedirectToAction("Index");
+            //}
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(p);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            var categories = GetProductCategories();
+            ViewBag.ListPCategory = new SelectList(categories, "PcategoryId", "PcategoryName");
+            return View(p);
         }
     }
 }
