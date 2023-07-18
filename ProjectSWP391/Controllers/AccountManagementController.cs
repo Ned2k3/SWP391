03e6238041ecfbb02c7a0a42fbc5b34d75d83bb8
Longcodebad <Longcodebad@gmail.com>
2023-07-18 20:35:29 +0700

    - Commit service product record

---
+++ ProjectSWP391/Controllers/AccountManagementController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using ProjectSWP391.Models;
+using System.Security.Principal;
+
+namespace ProjectSWP391.Controllers
+{
+    public class AccountManagementController : Controller
+    {
+        private readonly SWP391_V4Context context;
+
+
+        public AccountManagementController(SWP391_V4Context _context)
+        {
+            context = _context;
+        }
+
+        public IActionResult AccountList()
+        {
+            List<Account> accounts = context.Accounts.ToList();
+            return View(accounts);
+            
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                ViewBag.ErrorMsg = "You cant change this user";
+                return View();
+            }
+            var a = context.Accounts.Where(ac => ac.AccountId == id).SingleOrDefault();
+            context.Remove(a);
+            context.SaveChanges();
+            return RedirectToAction("AccountList");
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            
+            if (id == 0)
+            {
+                ViewBag.ErrorMsg = "You cant change this user";
+                return View();
+            }
+            var a = context.Accounts.SingleOrDefault(v => v.AccountId == id);
+            return View(a);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                
+                if (account.Role == 3)
+                {
+                    account.Role = null;
+                }
+                account.Password = EncryptionHelper.Encrypt(account.Password);
+                context.Update(account);
+                context.SaveChanges();
+                ViewBag.SuccessMsg = "SaveChange Successed";
+
+            }
+            return View(account);
+
+        }
+        public IActionResult Detail(string email)
+        {
+            var a = context.Accounts.SingleOrDefault(i => i.Email == email);
+            return View(a);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Account account)
+        {
+            if(account == null)
+            {
+                ViewBag.ErrorMsg = "Account input field";
+                return View();
+            }
+            else
+            {
+                var a = context.Accounts.Where(c => c.Email == account.Email).SingleOrDefault();
+                
+                if (a == null)
+                {
+                    account.Password = EncryptionHelper.Encrypt(account.Password);
+                    //let choose role
+                    if(account.Role == 3) { account.Role=null; }
+                    
+                    context.Add(account);
+                    context.SaveChanges();
+                }
+                return RedirectToAction("AccountList");
+            }
+        }
+        
+    }
+}
--- ProjectSWP391/Models/ExtendedModels/ServiceUserData.cs
+++ ProjectSWP391/Models/ExtendedModels/ServiceUserData.cs
@@ -1,7 +1,31 @@
-﻿namespace ProjectSWP391.Models.ExtendedModels
+﻿using ProjectSWP391.Models;
+
+namespace ProjectSWP391.Models.ExtendedModels
 {
     public class ServiceUserData
     {
+        public string CustomerEmail { get; set; }
+        public string EmployeeEmail { get; set; }
+        public int BookingId { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int Shift { get; set; }  
+        public string ServiceName { get; set; }
+        
+        public decimal Price { get; set; }
+        public string Note { get; set; }
 
+        public ServiceUserData(string customerEmail, string employeeEmail, int bookingId, DateTime bookingDate, int shift, string serviceName, decimal price, string note)
+        {
+            CustomerEmail = customerEmail;
+            EmployeeEmail = employeeEmail;
+            BookingId = bookingId;
+            BookingDate = bookingDate;
+            Shift = shift;
+            ServiceName = serviceName;
+            Price = price;
+            Note = note;
+        }
+
+      
     }
 }
