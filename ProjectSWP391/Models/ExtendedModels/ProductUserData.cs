494c9a112ce025d5449f4af286225765570a6874
longpthe153724 <pham.thanhlong666666@gmail.com>
2023-07-25 22:25:18 +0700

    Commit Final

--- ProjectSWP391/Models/ExtendedModels/ProductUserData.cs
+++ ProjectSWP391/Models/ExtendedModels/ProductUserData.cs
@@ -7,15 +7,17 @@
         public int OrderId { get; set; }
         public DateTime OrderDate { get; set; }
         public int Amount { get; set; }
+        public string? Content { get; set; }
         public decimal Total { get; set; }
 
-        public ProductUserData(string buyerEmail, string productName,int orderId, DateTime orderDate, int amount, decimal total)
+        public ProductUserData(string buyerEmail, string productName,int orderId, DateTime orderDate, int amount, string? content,decimal total)
         {
             BuyerEmail = buyerEmail;
             ProductName = productName;
             OrderId = orderId;
             OrderDate = orderDate;
             Amount = amount;
+            Content = content;
             Total = total;
         }
     }
